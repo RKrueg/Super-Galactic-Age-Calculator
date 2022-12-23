d2bdd1a955aff68153432e95fa064e6f974b4e1b
RKrueg <thegckid@gmail.com>
2022-12-23 00:00:11 -0800

    adjust test code so it will be easier to understand and connect to the getAge method

--- __tests__/calculateAge.test.js
+++ __tests__/calculateAge.test.js
@@ -1,8 +1,8 @@
-import Calculate from "../src/calculateAge";
+import { Calculate } from "../src/calculateAge";
 
 describe('Calculate', () => {
-  test('should correctly determine a persons age in solar years on Mercury', () => {
-    const age = new Calculate (32);
-    expect(age.mercuryAge).toEqual(133);
+  test('should correctly determine a persons age based on Mercurys solar years', () => {
+    let calculate = new Calculate(32)
+    expect(calculate.getAgeOnMercury()).toEqual(133);
   })
 })
