bc1e3bb07f20eda0a6e6f8b1d99e7570b9464726
RKrueg <thegckid@gmail.com>
2022-12-23 18:11:35 -0800

    test should determine how many years have passed on each planet since a past birthday

--- __tests__/calculateAge.test.js
+++ __tests__/calculateAge.test.js
@@ -2,19 +2,27 @@ import { Calculate } from "../src/calculateAge";
 
 describe('Calculate', () => {
   test('should correctly determine a persons age based on Mercurys solar years', () => {
-    let calculate = new Calculate(32)
-    expect(calculate.getAgeOnMercury()).toEqual(133);
+    let calculateAge = new Calculate(32)
+    expect(calculateAge.getAgeOnMercury()).toEqual(133);
   })
   test('should correctly determine a persons age based on Venus solar year', ()  => {
-    let calculate = new Calculate(32)
-    expect(calculate.getAgeOnVenus()).toEqual(51)
+    let calculateAge = new Calculate(32)
+    expect(calculateAge.getAgeOnVenus()).toEqual(51)
   })
   test('should correctly determine a persons age based on Mars solar year', () => {
-    let calculate = new Calculate(32)
-    expect(calculate.getAgeOnMars()).toEqual(17)
+    let calculateAge = new Calculate(32)
+    expect(calculateAge.getAgeOnMars()).toEqual(17)
   })
   test('should correctly determine a persons age based on Jupiters solar year', () => {
-    let calculate = new Calculate(32)
-    expect(calculate.getAgeOnJupiter()).toEqual(2)
+    let calculateAge = new Calculate(32)
+    expect(calculateAge.getAgeOnJupiter()).toEqual(2)
+  })
+  test('should determine how many years have passed on each planet since a past birthday', () => {
+    let calculateAge = new Calculate(32)
+    expect(calculateAge.getYearsPassedEarth()).toEqual(10)
+    expect(calculateAge.getYearsPassedMercury()).toEqual(41)
+    expect(calculateAge.getYearsPassedVenus()).toEqual(16)
+    expect(calculateAge.getYearsPassedMars()).toEqual(6)
+    expect(calculateAge.getYearsPassedJupiter()).toEqual(1)
   })
 })
